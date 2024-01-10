c1ddd91db6fa56863331b4c9bbe14facaaa5c717
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-10 11:55:03 +0900

    aa

--- src/racehorse.php
+++ src/racehorse.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php require 'db-connect.php';?>
+<?php require 'header.php'; ?>
+<?php
+$pdo=new PDO($connect, USER, PASS);
+foreach($pdo->query('select * from Racehorse') as $row){
+    echo '<p>';
+    echo $row['recehorse_name'],':';
+    echo $row['fathername'],':';
+    echo $row['gender'],':';
+    echo $row['bithday'],':';
+    echo '</p>';
+}
+
+?>
+<?php require 'footer.php'; ?>
