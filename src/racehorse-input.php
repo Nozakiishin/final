aa4aef05bed0dc3195b41c849ec44e29fa1d4c02
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-10 12:20:56 +0900

    aa

--- src/racehorse-input.php
+++ src/racehorse-input.php
@@ -4,7 +4,7 @@
 競走馬名<input type="text" name="recehorse_name"><br>
 父名<input type="text" name="fathername"><br>
 性別<input type="text" name="gender"><br>
-年月日<input type="text" name="birthday"><br>
+年月日<input type="date" name="birthday"><br>
 
 </form>
 <?php require 'footer.php'; ?>
--- src/racehorse_output.php
+++ src/racehorse_output.php
@@ -4,8 +4,17 @@
 $pdo=new PDO($connect, USER, PASS);
 $sql=$pdo->prepare('insert into Racehorse(racehorse_name,fathername,gender,birthday) values (?,?,?,?,)');
 if(empty($POST['recehorse_name'])){
-    echo '競走馬名名を入力してください';
-    
+    echo '競走馬名を入力してください';   
+}else if(empty($POST['fathername'])){
+    echo '父名を入力してください';   
+}else if(empty($POST['gender'])){
+    echo '性別を入力してください';   
+}else if(empty($POST['birthday'])){
+    echo '年月日を入力してください';   
+}else if($sql->execute([$_POST['recehorse_name'],$POST['fathername'],$POST['gender'],$POST['birthday']])){
+    echo '追加に成功しました。';   
+}else {
+    echo '追加に失敗しました。'
 }
 ?>
 <?php require 'footer.php'; ?>
