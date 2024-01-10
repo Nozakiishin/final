5bf2c6a31c341b9b1b013435a9344faab4733708
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-10 11:43:24 +0900

    aa

---
+++ src/db-conect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517446-final';
+    const USER = 'LAA1517446';
+    const PASS = 'Pass0903';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
