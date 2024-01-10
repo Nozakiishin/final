c3bb1d0fd7f381a1ebd3c5c2ade099917ea7cc46
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-10 12:29:16 +0900

    a

--- src/racehorse-input.php
+++ src/racehorse-input.php
@@ -1,6 +1,6 @@
 <?php require 'header.php'; ?>
 <p>競走馬情報を登録します</p>
-<form action="recehorse-output.php" method="post">
+<form action="racehorse-output.php" method="post">
 競走馬名<input type="text" name="recehorse_name"><br>
 父名<input type="text" name="fathername"><br>
 性別<input type="text" name="gender"><br>
