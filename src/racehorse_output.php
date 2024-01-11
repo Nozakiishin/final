47cd98c58e7e2574ccb4f76ded37316fd50ae143
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-11 11:53:09 +0900

    a

--- src/racehorse_output.php
+++ src/racehorse_output.php
@@ -3,14 +3,14 @@
 <?php
 $pdo=new PDO($connect, USER, PASS);
 $sql=$pdo->prepare('insert into Racehorse(racehorse_id,racehorse_name,fathername,gender,birthday) values (default,?,?,?,?)');
-if(empty($POST['racehorse_name'])){
-    echo '競走馬名を入力してください';   
-}else if(empty($POST['fathername'])){
-    echo '父名を入力してください';   
-}else if(empty($POST['gender'])){
-    echo '性別を入力してください';   
-}else if(empty($POST['birthday'])){
-    echo '年月日を入力してください';   
+// if(empty($POST['racehorse_name'])){
+//     echo '競走馬名を入力してください';   
+// }else if(empty($POST['fathername'])){
+//     echo '父名を入力してください';   
+// }else if(empty($POST['gender'])){
+//     echo '性別を入力してください';   
+// }else if(empty($POST['birthday'])){
+//     echo '年月日を入力してください';   
 }else if($sql->execute([$_POST['racehorse_id'],$_POST['racehorse_name'],$POST['fathername'],$POST['gender'],$POST['birthday']])){
     echo '追加しました。';   
 }else {
