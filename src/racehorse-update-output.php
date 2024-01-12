e957ad869005ad5948664bd60bd81f65152e8d0f
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-12 12:49:01 +0900

    a

--- src/racehorse-update-output.php
+++ src/racehorse-update-output.php
@@ -2,7 +2,7 @@
 <?php require 'header.php'; ?>
 <?php
 $pdo=new PDO($connect, USER, PASS);
-$sql=$pdo->prepare('update Racehorse set racehorse_name=?, fathername=? gender=?,birthday=? where racehorse_id=? ');
+$sql=$pdo->prepare('update Racehorse set racehorse_name=?, fathername=? ,gender=?,birthday=? where racehorse_id=? ');
 // if(empty($POST['racehorse_name'])){
 //     echo '競走馬名を入力してください';   
 // }else if(empty($POST['fathername'])){
