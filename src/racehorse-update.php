c0e07ed85ab1acc20c52facda88380902a51df25
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-01-16 11:25:09 +0900

    a

--- src/racehorse-update.php
+++ src/racehorse-update.php
@@ -1,9 +1,5 @@
 <?php require 'db-connect.php';?>
 <?php require 'header.php'; ?>
-	<div class="th1">競走馬名</div>
-	<div class="th1">父名</div>
-    <div class="th1">性別</div>
-	<div class="th1">年月日</div>
 
 <?php
     $pdo=new PDO($connect, USER, PASS);
@@ -16,19 +12,19 @@
 		echo '<div class="td0">',$row['racehorse_id'],'</div>';
 
 		echo '<div class="td1">';
-		echo '<input type="text" name="racehorse_name" value"',$row['racehorse_name'],'">';
+		echo '競走馬名:<input type="text" name="racehorse_name" value"',$row['racehorse_name'],'">';
 		echo '</div> ';
 
 		echo '<div class="td1">';
-		echo '<input type="text" name="fathername" value"',$row['fathername'],'">';
+		echo '父名:<input type="text" name="fathername" value"',$row['fathername'],'">';
 		echo '</div> ';
 
         echo '<div class="td1">';
-		echo '<input type="text" name="gender" value"',$row['gender'],'">';
+		echo '性別:<input type="text" name="gender" value"',$row['gender'],'">';
 		echo '</div> ';
 
 		echo '<div class="td1">';
-		echo '<input type="date" name="birthday" value"',$row['birthday'],'">';
+		echo '年月日:<input type="date" name="birthday" value"',$row['birthday'],'">';
 		echo '</div> ';
 
 	    echo '<div class="td1">';
